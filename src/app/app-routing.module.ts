001550898ade238129787812dd14419572ad78cc
rquirogagUcentral <rquirogag@ucentral.edu.co>
2021-03-17 00:03:58 -0500

    Creación primeros componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RegisterComponent } from './components/register/register.component';
+import { SessionComponent } from './components/session/session.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: 'login', component: SessionComponent},
+  {path: 'register', component: RegisterComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavegationComponent } from './components/navegation/navegation.component';
+import { SessionComponent } from './components/session/session.component';
+import { RegisterComponent } from './components/register/register.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { FinderComponent } from './components/finder/finder.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavegationComponent,
+    SessionComponent,
+    RegisterComponent,
+    BannerComponent,
+    FinderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
