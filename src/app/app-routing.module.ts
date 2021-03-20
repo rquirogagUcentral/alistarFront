11e67b73b00ba0c4f9c2c5474309418658e5a541
cesar <caacesar.ariza@gmail.com>
2021-03-19 22:12:35 -0500

    se hace  los cambios y modificaciones

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { Component, NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ComponenteInicialComponent } from './components/componente-inicial/componente-inicial.component';
 import { RegisterComponent } from './components/register/register.component';
 import { SessionComponent } from './components/session/session.component';
 
 
 const routes: Routes = [
   {path: 'login', component: SessionComponent},
-  {path: 'register', component: RegisterComponent}
+  {path: 'register', component: RegisterComponent},
+  {path: '',component:ComponenteInicialComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { RegisterComponent } from './components/register/register.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { FinderComponent } from './components/finder/finder.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { ComponenteInicialComponent } from './components/componente-inicial/componente-inicial.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { FooterComponent } from './components/footer/footer.component';
     RegisterComponent,
     BannerComponent,
     FinderComponent,
-    FooterComponent
+    FooterComponent,
+    ComponenteInicialComponent
   ],
   imports: [
     BrowserModule,
