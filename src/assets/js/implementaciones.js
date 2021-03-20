8552960f5f0b8477633cf1af7176ba0e585a783f
cesar <caacesar.ariza@gmail.com>
2021-03-19 22:49:26 -0500

    se cambiea el baner y se ajusta las imagenes

--- src/assets/js/implementaciones.js
+++ src/assets/js/implementaciones.js
@@ -1,15 +1,11 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.carousel');
-  var instance = M.Carousel.init({
-    fullWidth: true
-  });
-});
+
 
 // Or with jQuery
 
 $(document).ready(function(){
   $('.carousel.carousel-slider').carousel({
-    fullWidth: true
+    fullWidth: true,
+    indicators: true
   });
 });
 
