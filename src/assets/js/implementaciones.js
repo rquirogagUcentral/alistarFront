d6a34e8763f75cc11b4345f0782c972a3d058ff6
cesar <caacesar.ariza@gmail.com>
2021-05-07 23:11:13 -0500

    se verifica la parte de registro y se realiza el adecuamiento del front

--- src/assets/js/implementaciones.js
+++ src/assets/js/implementaciones.js
@@ -1,27 +1,5 @@
 
-
-// Or with jQuery
-
-$(document).ready(function(){
-  $('.carousel.carousel-slider').carousel({
-    fullWidth: true,
-    indicators: true
-  });
-});
-
-document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('select');
-  var instances = M.FormSelect.init(elems, options);
-  throw new Error('Valid token not returned')
-});
-
-$(document).ready(function(){
-  $('select').formSelect();
-});
-
 $(document).ready(function(){
   $('.datepicker').datepicker();
+  $('.modal').modal();
 });
-
-// Or with jQuery
-
