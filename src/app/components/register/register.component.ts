ed5bc27812522a516520e0c7ff218d96e5807ce3
cesar <caacesar.ariza@gmail.com>
2021-05-10 00:07:09 -0500

    se verifica la parte de registro y se realiza el adecuamiento del front

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -12,6 +12,7 @@ export class RegisterComponent implements OnInit {
 
   constructor() {
     console.log(this.docs);
+
     `$('.datepicker').datepicker()`;
     `$('.modal').modal()`;
     `$('select').formSelect()`;
--- src/assets/js/implementaciones.js
+++ src/assets/js/implementaciones.js
@@ -3,4 +3,16 @@ $(document).ready(function(){
   $('.datepicker').datepicker();
   $('.modal').modal();
   $('select').formSelect();
+
 });
+document.addEventListener('click', function() {
+  var elems = document.querySelectorAll('.modal');
+  var instances = M.Modal.init(elems, options);
+});
+
+function select(){
+  $('select').formSelect();
+}
+function modal(){
+  $('.modal').modal();
+}
